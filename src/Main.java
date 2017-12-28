02f5fca0a58149c15f5c11a0f265bb43eab0b116
posusaevkostay@mail.ru <posusaevkostay@mail.ru>
2017-12-28 22:48:55 +0300

    Task24

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i < 110; i = i + 2) {
+            count++;
+            System.out.println(i);
+        }System.out.println(count);
+    }
+}
